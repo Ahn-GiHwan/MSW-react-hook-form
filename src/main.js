eeee9ca902bf028b6f6db787e0e53d8d39eb314a
Ahn-Gihwan <ahngh0113@naver.com>
2021-12-29 18:31:35 +0900

    feat: use msw

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import App from "./App.jsx";
 
 import "./css/css.css";
 
+if (process.env.NODE_ENV === "development") {
+  const { worker } = require("./mocks/browser");
+  worker.start();
+}
+
 const root = document.getElementById("root");
 
 ReactDOM.render(<App />, root);
