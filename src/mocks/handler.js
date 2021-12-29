bae3542a1dad24bd32e8eb53de07c2db9f313b83
Ahn-Gihwan <ahngh0113@naver.com>
2021-12-29 18:31:49 +0900

    feat: 사용할 rest api 설정

---
+++ src/mocks/handler.js
@@ -0,0 +1,23 @@
+import { rest } from "msw";
+
+export const handler = [
+  rest.post("/login", async (req, res, ctx) => {
+    const { id, pw } = req.body;
+
+    if (id === "1" && pw === "2") {
+      return res(
+        ctx.json({
+          result: true,
+          text: `Welcome [${id}]님!`,
+        })
+      );
+    } else {
+      return res(
+        ctx.json({
+          result: false,
+          text: "아이디 비번을 확인하세요",
+        })
+      );
+    }
+  }),
+];
