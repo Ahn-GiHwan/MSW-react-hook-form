a5b4b8c505128aea3e0b71cfec8b98ec17fd2ba3
Ahn-Gihwan <ahngh0113@naver.com>
2021-12-29 23:37:18 +0900

    style: import bootstrap css

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 
 import App from "./App.jsx";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/css.css";
 
 if (process.env.NODE_ENV === "development") {
